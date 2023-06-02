b89f023cdac44241b93e8b0c39b2cbfa3c35a5f7
krippergo <99502466+krippergo@users.noreply.github.com>
2023-06-02 21:33:36 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+let express = require(`express`);
+let app = express();
+let port = 443;
+
+app.listen(port, function () {
+    console.log(`http://localhost:${port}`);
+})
+
+// Раздача статики
+app.use(express.static(`public`));
+
+app.get('/', function (req, res) {
+    let ip = req.ip;
+
+    res.send(ip);
+});
